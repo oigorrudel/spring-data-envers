7f347bb22cae6051185889d117567da789f9565d
Igor Rudel <igor.rudel@realizecfi.com.br>
2025-03-25 18:28:40 -0300

    Separando impls em modulos

---
+++ spring3-data-envers/src/main/java/br/xksoberbado/spring3dataenvers/Spring3DataEnversApplication.java
@@ -0,0 +1,13 @@
+package br.xksoberbado.spring3dataenvers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3DataEnversApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3DataEnversApplication.class, args);
+    }
+
+}
---
+++ spring3-data-envers/src/test/java/br/xksoberbado/spring3dataenvers/Spring3DataEnversApplicationTests.java
@@ -0,0 +1,13 @@
+package br.xksoberbado.spring3dataenvers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3DataEnversApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
